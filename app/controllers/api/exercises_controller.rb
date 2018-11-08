93881a1809f975eb9037a6783854ee59072b0984
Alan <alanbuckle05@gmail.com>
2018-11-08 14:22:10 -0500

    new controllers set up

--- app/controllers/api/exercises_controller.rb
+++ app/controllers/api/exercises_controller.rb
@@ -1,2 +1,32 @@
 class Api::ExercisesController < ApplicationController
+    def index
+        @exercises = Exercise.all
+        render json: @exercises
+    end
+
+    def show
+        @exercise = Exercise.find(params[:id])
+        render json: @exercise
+    end
+
+    def create
+        @exercise = Exercise.create(exercise_params)
+        render json: @exercise
+    end
+
+    def update
+        @exercise = Exercise.update(params[:id])
+        render json: @exercise
+    end
+
+    def destroy
+        @exercise = Exercise.find(params[:id]).delete
+        render status: :ok
+    end
+
+    private 
+    
+    def exercise_params
+        params.require(:exercise).permit(:name, :muscles)
+    end
 end
---
+++ app/controllers/api/workout_exercises_controller.rb
@@ -0,0 +1,3 @@
+class Api::WorkoutExercisesController < ApplicationController
+    
+end
