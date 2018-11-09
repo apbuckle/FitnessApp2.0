e0b4cde92755935c92f8a93ea2c77d3b9e3129d0
Alan <alanbuckle05@gmail.com>
2018-11-09 15:39:50 -0500

    addded data to seeds and able to fully render a workout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
 # create all workouts
 legs = Workout.create(
-    title: "Leg Day",
+    title: "Tree Trunks",
     day: "Monday and Friday"
 )
 
-upperBody = Workout.create(
-    title: "Swole Patrol",
+chest = Workout.create(
+    title: "Square Chest",
     day: "Tuesday and Saturday"
 )
+
+back = Workout.create(
+    title: "Cobra Back",
+    day: "Wednesday and Sunday"
+)
+
+core = Workout.create(
+    title: "Core Strength",
+    day: "Monday, Wednesday, Friday"
+)
+
 
 # create all exercises
 squats = Exercise.create(
@@ -34,10 +36,79 @@ lunges = Exercise.create(
     muscles: "Glutes, Quads, Hamstrings"
 )
 
-# create all joins
-leg_squats = WorkoutExercise.create(
-    {workout: legs, exercise: squats, duration: 20, sets: 3})
-
-swole_Patrol = WorkoutExercise.create(
-    {workout: upperBody, exercise: deadlifts, duration: 10, sets: 5}
+leg_press = Exercise.create(
+    name: "Leg Press",
+    muscles: "Glutes, Quads, Hamstrings"
 )
+
+leg_extension = Exercise.create(
+    name: "Leg Extension",
+    muscles: "Quads"
+)
+
+leg_curl = Exercise.create(
+    name: "Prone Leg Curl",
+    muscles: "Hamstrings, Glutes, Calves"
+)
+
+incline_press = Exercise.create(
+    name: "Dumbbell Incline Press",
+    muscles: "Upper Pectorial, Anterior Deltiod, Triceps"
+)
+
+bench_press = Exercise.create(
+    name: "Flat Bench Press",
+    muscles: "Pectorial Major and Minor, Anterior Deltoid, Triceps"
+)
+
+decline_press = Exercise.create(
+    name: "Decline Bench Press",
+    muscles: "Lower Pectorial, Triceps"
+)
+
+dips = Exercise.create(
+    name: "Dips",
+    muscles: "Lower Pectorial, Triceps"
+)
+
+flys = Exercise.create(
+    name: "Cable Flys",
+    muscles: "Pectorial Major"
+)
+
+# create all joins
+leg_exer_one = WorkoutExercise.create(
+    {workout: legs,
+    exercise: squats,
+    duration: 10,
+    sets: 4})
+    
+leg_exer_two = WorkoutExercise.create(
+    {workout: legs,
+    exercise: leg_press,
+    duration: 10,
+    sets: 4})
+
+leg_exer_three = WorkoutExercise.create(
+        {workout: legs,
+        exercise: leg_curl,
+        duration: 10,
+        sets: 3})
+
+leg_exer_four = WorkoutExercise.create(
+    {workout: legs,
+    exercise: leg_extension,
+    duration: 10,
+    sets: 3})
+
+       
+    
+
+
+
+
+
+
+# chest = WorkoutExercise.create(
+#     {workout: chest, exercise: incline_press, flys, dips, duration: 8-12, sets: 4}
+# )
